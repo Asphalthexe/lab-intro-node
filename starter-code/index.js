85781b72a3fbdb1c16e1f89c861efc2e8f4dabe4
Sonja Lesch <sonjalesch@gmx.de>
2020-03-31 19:16:30 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b) {
+      return a-b; // muss sein, weil javascript mit sort() nur bei strings funktioniert, nicht bei Nummern, weil es die Nummern in einen string konvertiert
+      //shorter: this.items.sort((a, b) => a - b);
+    });
+    this.length++; // or this.length += 1;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length -1) {
+    throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) { throw new Error(`EmptySortedList`) }
+    return this.items[this.items.length -1];
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) { throw new Error(`EmptySortedList`) }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) { return 0 }
+    return this.items.reduce(function(a, b) {
+      return a + b;
+    });
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) { throw new Error(`EmptySortedList`) }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
+
+
+
